88503987aaa2e3ca2ed709ec334f7231193c927b
DeadpanSplash82 <u21434159@tuks.co.za>
2022-05-05 10:08:30 +0200

    Implement KILL, LIST & QUIT

--- client_src/index.js
+++ client_src/index.js
@@ -1,11 +1,22 @@
-const PORT = 8800;
+const PORT = 8321;
 var socket = new WebSocket('ws://localhost:' + PORT);
 socket.onopen = function () {
     socket.send('hello');
     $("#data").append("Connected<br/>");
     $("#data").append("Sending Hello <br/>");
+
+    $("#send").click(function (e) {
+        e.preventDefault;
+        var text = $("#message").val();
+        socket.send(text);
+        $("#data").append("Sending " + text + " <br/>");
+    });
 };
 socket.onmessage = function (s) {
     console.log(s);
     $("#data").append("Reply: " + s.data + "<br/><br/>");
 };
+
+socket.onclose = function (e) {
+    $("#data").append("Connection lost<br/><br/>");
+}
--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 const http = require('http');
 const fs = require('fs');
 const WebSocket = require('ws');
+const events = require("events");
 const clientFiles = ['/index.css', '/index.html', '/', '/index.js', '/favicon.ico'];
+const clients = new Set();
 
 // const prompt = require("prompt-sync")({ sigint: true });
 // const PORT = prompt("Which port should the server use? ");
@@ -21,13 +23,13 @@ const server = http.createServer(function (req, res) {
             res.write(f);
             res.end();
         }
-        else {
-            console.log(req.url + ' not found, returning 404');
-            res.writeHead(404, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
-            res.write('{"status" : "failed", "error" : "Not found"}');
-            res.end();
-            return;
-        }
+        // else {
+        //     console.log(req.url + ' not found, returning 200');
+        //     res.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+        //     res.write('{"status" : "failed", "error" : "Not found"}');
+        //     res.end();
+        //     return;
+        // }
     }
 
 }).listen(PORT);
@@ -40,12 +42,19 @@ server.on('upgrade', function (request, socket, head) {
     })
 })
 wss.on('connection', ws => {
-    console.log('user is connected to socket');
+    ws.id = "User" + (clients.size + 1);
+    clients.add(ws);
+    console.log(ws.id + ' is connected to socket');
     ws.on('message', function incoming(data) {
         console.log('Message received: ' + data);
-        ws.send("Message received by server");
+        for (let c of clients)
+            c.send("Message of " + data.toString().trim() + " from " + ws.id + " acknowledged by server");
     });
 
+    ws.on('close', function incoming(data) {
+        console.log(ws.id + ' has disconnected');
+        clients.delete(ws);
+    });
 });
 
 
@@ -70,3 +79,67 @@ function getFile(name) {
 
 }
 
+process.stdin.addListener('data', data => {
+    strdata = data.toString();
+    // console.log(strdata.includes("KILL"));
+    if (isEq(strdata, "LIST"))
+        commandList();
+    else if (isEq(strdata, "QUIT"))
+        commandQuit();
+    else if (strdata.includes("KILL"))
+        commandKill(strdata);
+    else console.log('Unrecognised command');
+})
+
+function commandList() {
+    console.log("All active connections:")
+    if (clients.size == 0)
+        console.log("none");
+    else
+        for (let c of clients)
+            console.log(c.id);
+}
+
+function commandKill(c) {
+    if (clients.size == 0) {
+        console.log("There are currently no connected users")
+        return;
+    }
+    id = c.replace("KILL ", "").trim();
+    for (let c of clients) {
+        if (c.id === id) {
+            c.send("You are being disconnected");
+            c.close();
+            clients.delete(c);
+            return;
+        }
+    }
+    console.log("User not found");
+}
+
+function commandQuit() {
+    if (clients.size == 0)
+        return;
+    for (let c of clients) {
+        c.send("This server will be going offline now and thus you will be disconnected");
+        c.close();
+        clients.delete(c);
+    }
+    console.log("All users have been disconnected and server will now go offline");
+    process.exit();
+}
+
+function isEq(a, b) {
+    var i = 0;
+    var j = 0;
+    var result = true;
+    while (j < b.length) {
+        if (a[i] != b[j] || i == a.length) {
+            result = false;
+        }
+        else
+            i++;
+        j++;
+    }
+    return result;
+}
